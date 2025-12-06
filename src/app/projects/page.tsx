7baafa0657a206f320d36e7a378073d008276b81
EmilioCharles <emiliocharlescontact@gmail.com>
2025-12-06 11:28:28 +0200

    fix: adjust phone mockup mobile layout (width 25%, right aligned)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -77,7 +77,7 @@ export default function ProjectsPage() {
                                 </div>
 
                                 {/* Phone - Visible on all screens, adjusted positioning */}
-                                <div className="absolute -right-2 -bottom-4 md:-bottom-6 w-[15%] md:w-[20%] z-20 transform group-hover:translate-x-2 transition-transform duration-500">
+                                <div className="absolute right-0 -bottom-12 md:-right-2 md:-bottom-6 w-[25%] md:w-[20%] z-20 transform group-hover:translate-x-2 transition-transform duration-500">
                                     <PhoneMockup>
                                         <img
                                             src={project.mobileImg}
