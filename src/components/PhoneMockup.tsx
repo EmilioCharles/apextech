99a7c67d5cac819ce6d804ed183efd2225d0f7a8
EmilioCharles <emiliocharlescontact@gmail.com>
2025-12-06 12:10:55 +0200

    style: refine phone mockup to match iPhone 15 Pro Max aesthetic

--- src/components/PhoneMockup.tsx
+++ src/components/PhoneMockup.tsx
@@ -7,14 +7,14 @@ interface PhoneMockupProps {
 export default function PhoneMockup({ children }: PhoneMockupProps) {
     return (
         <div className="relative mx-auto w-full">
-            <div className="relative rounded-[2.5rem] bg-[#1a1a1a] p-2 shadow-xl border border-white/10 ring-1 ring-black">
-                {/* Camera / Notch Area */}
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 h-6 w-32 bg-black rounded-b-xl z-20 flex justify-center items-center">
-                    <div className="w-16 h-1 bg-[#222] rounded-full opacity-50" />
+            <div className="relative rounded-[2.2rem] bg-[#1f1f1f] p-1.5 shadow-xl border border-gray-800 ring-1 ring-black">
+                {/* Dynamic Island */}
+                <div className="absolute top-2 left-1/2 -translate-x-1/2 h-[3.5%] w-[30%] bg-black rounded-full z-20 flex justify-center items-center">
+                    <div className="w-1/3 h-1/3 bg-[#111] rounded-full opacity-60 ml-[60%]" />
                 </div>
 
                 {/* Screen */}
-                <div className="relative rounded-[2rem] bg-black overflow-hidden aspect-[9/19] border border-white/5">
+                <div className="relative rounded-[1.8rem] bg-black overflow-hidden aspect-[9/19.5] border border-white/5">
                     {children}
 
                     {/* Screen Glare */}
